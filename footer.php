b2ca8ae765cae984b1d082d9bcf366aabe70da85
Leslie Goh <53505899+lesliegoh@users.noreply.github.com>
2021-05-18 15:05:15 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -13,5 +13,5 @@
 </div>
 </div>
 </footer>
-<script src="assets/jquery-3.4.0/jquery.min.js"></script>
+<script src="assets/jquery-3.4.1/jquery-3.4.1.min.js"></script>
 <script src="assets/bootstrap-4.3.1/js/bootstrap.bundle.min.js"></script>
